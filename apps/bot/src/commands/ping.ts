d74dd0908a913245a396aae16f1b496af0270c0d
arsabutispik <ispik6441@proton.me>
2026-02-16 14:47:12 +0300

    Enhance PingCommand response to include latency information

--- apps/bot/src/commands/ping.ts
+++ apps/bot/src/commands/ping.ts
@@ -1,11 +1,11 @@
-import { Command, BaseCommand, Context } from "@marshmallow/mally";
-//Unrelated message for testing purposes
+import {BaseCommand, Command, Context} from "@marshmallow/mally";
+
 @Command({
     description: "Replies with Pong!",
     name: "ping",
 })
 export class PingCommand extends BaseCommand {
     async run(ctx: Context) {
-        await ctx.reply("Pong!");
+        await ctx.reply( `Pong! Latency: ${Date.now() - ctx.message.createdAt.getTime()}ms` );
     }
 }
