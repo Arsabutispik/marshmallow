e2b20ab05defd1b123be9e78efe425363d88f2a9
Arsabutispik <ispik@ispik.dev>
2026-05-11 16:03:09 +0300

    feat: add missing exports for RoleManager, ServerChannelManager, DMChannel, and permissions utility
    Signed-off-by: Arsabutispik <ispik@ispik.dev>

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -6,17 +6,21 @@ export * from "./gateway/GatewayManager";
 export * from "./managers/ChannelManager";
 export * from "./managers/MemberManager";
 export * from "./managers/MessageManager";
+export * from "./managers/RoleManager"
+export * from "./managers/ServerChannelManager";
 export * from "./managers/ServerManager";
+export * from "./managers/SweepManager"
 export * from "./managers/UserManager";
 
 // Structures
 export * from "./structures/Attachment";
 export * from "./structures/Base";
 export * from "./structures/BaseChannel";
+export * from "./structures/DMChannel";
 export * from "./structures/Member";
 export * from "./structures/Message";
+export * from "./structures/Role";
 export * from "./structures/Server";
-export * from "./structures/TextChannel";
 export * from "./structures/TextChannel";
 export * from "./structures/UnknownChannel";
 export * from "./structures/User";
@@ -26,3 +30,4 @@ export * from "./builders/EmbedBuilder";
 
 // Utils
 export * from "./utils/Collection";
+export * from "./utils/permissions";
